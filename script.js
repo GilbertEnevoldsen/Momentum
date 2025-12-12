581899486d8becc107610f3f47d83fece2f43eba
GilbertEnevoldsen <gilbertenevoldsen@gmail.com>
2025-12-12 12:22:43 +0100

    Added working nav bar

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const calender = [2, 5, 8, 10, 12, 15, 18, 20, 22, 25, 27];
+var state = "home";
+
+const calender = [2, 5, 8, 10, 12, 15, 18, 20, 22, 25, 27]; // temp data
 
 document.getElementById("calender").innerHTML += generateCalender();
+
+function navigate(to) {
+    document.getElementById("nav-menu").querySelector("#" + state).classList.remove("active");
+    document.getElementById("nav-menu").querySelector("#" + to).classList.add("active");
+    document.querySelector("section#" + state).classList.remove("active");
+    document.querySelector("section#" + to).classList.add("active");
+    state = to;
+}
 
 function generateCalender() {
     let html = "";
@@ -17,3 +27,5 @@ function generateCalender() {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
+
+navigate("statistics");
